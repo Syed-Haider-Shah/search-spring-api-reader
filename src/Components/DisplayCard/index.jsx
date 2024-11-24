720c34f12cc6267afe3da3824dd02f26e5cbc2d1
Syed-Haider-Shah <haidershah123123123@gmail.com>
2024-11-24 23:24:41 +1100

    Skeleton removed

--- src/Components/DisplayCard/index.jsx
+++ src/Components/DisplayCard/index.jsx
@@ -1,18 +1,12 @@
 import PropTypes from 'prop-types';
-import Skeleton from 'react-loading-skeleton';
-import 'react-loading-skeleton/dist/skeleton.css'; // Ensure you import the CSS for the skeleton
 
 const DisplayCard = ({ name, msrp, price, image }) => {
   return (
     <div className='card'>
-      {image ? (
         <img src={image} alt="Item Image" className='image' />
-      ) : (
-        <Skeleton height={200} width={200} />
-      )}
-      <div className='name'>{name || <Skeleton width={100} />}</div>
+      <div className='name'>{name}</div>
 
-      {price !== undefined && msrp !== undefined ? (
+      {
         price <= msrp ? (
           <div className='price'>${price}</div>
         ) : (
@@ -20,10 +14,8 @@ const DisplayCard = ({ name, msrp, price, image }) => {
             <div className='slashed'>${price}</div>
             <div className='price'>${msrp}</div>
           </div>
-        )
-      ) : (
-        <Skeleton width={50} />
-      )}
+        )}
+
     </div>
   );
 };
